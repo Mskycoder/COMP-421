b7a17d867720e4a227015a0f351ab24f27e3e7b2
Mskycoder <gnidihz@gmail.com>
2019-03-25 23:08:34 -0400

    style update + clean unfinished features

--- app.py
+++ app.py
@@ -8,6 +8,7 @@ import plotly.graph_objs as go
 import plotly.plotly as py
 from datetime import datetime as dt
 import json
+from math import ceil
 
 app = dash.Dash(__name__)
 server = app.server
@@ -16,70 +17,12 @@ pharmacies = pd.read_csv("data/pharmacies.csv")
 prescriptionOfDrugs = pd.read_csv("data/prescriptionofdrugs.csv")
 prescriptions = pd.read_csv("data/prescriptions.csv")
 
-layout = dict(
-    autosize=True,
-    margin=dict(
-        l=45,
-        r=45,
-        b=45,
-        t=55
-    ),
-    hovermode="closest",
-    plot_bgcolor="#F9F9F9",
-    paper_bgcolor="#F9F9F9",
-    legend=dict(font=dict(size=10), orientation='h'),
-)
 
 # Standard Dash app code below
 app.layout = html.Div(className='container', children=[
     Header("Pharma421"),
     html.Pre(id='selected-data'),
-    Row(
-        style={
-            "display": "flex"
-        },
-        children=[
-            html.Div(
-                style={
-                    "flex" : "1"
-                },
-                className="pretty_container",
-                children = [
-                    html.P(""),
-                    html.H6(
-                        id="info1",
-                        className="info_text"
-                    )
-                ]
-            ),
-            html.Div(
-                style={
-                    "flex" : "1"
-                },
-                className="pretty_container",
-                children = [
-                    html.P(""),
-                    html.H6(
-                        id="info2",
-                        className="info_text"
-                    )
-                ]
-            ),
-            html.Div(
-                style={
-                    "flex" : "1"
-                },
-                className="pretty_container",
-                children = [
-                    html.P(""),
-                    html.H6(
-                        id="info3",
-                        className="info_text"
-                    )
-                ]
-            )
-        ]
-    ),
+
     Row(
         children = [
             html.Div(
@@ -94,9 +37,19 @@ app.layout = html.Div(className='container', children=[
                                     y=[len([True for date in prescriptions["prescription_date"] if (dt.strptime(date, "%Y-%m-%d").month==i)]) for i in range(1, 13)]
                                 )
                             ],
-                            "layout": {
-                                "title": "Monhtly Prescription Count"
-                            }
+                            "layout": dict(
+                                title="Monthly Prescription Count",
+                                autosize=True,
+                                margin=dict(
+                                    l=45,
+                                    r=45,
+                                    b=45,
+                                    t=55
+                                ),
+                                hovermode="closest",
+                                plot_bgcolor="#F9F9F9",
+                                paper_bgcolor="#F9F9F9",
+                            )
                         }
                     )
                 ]
@@ -112,34 +65,13 @@ app.layout = html.Div(className='container', children=[
 
 ])
 
-def filterDf(start, end):
-    start=dt.strptime(start, "%Y-%m/%d")
-    end=dt.strptime(end, "%Y-%m-%d")
-    return prescriptions[dt.strptime(prescriptions["prescription_date"], "%Y-%m-%d") > start
-        and dt.strptime(prescriptions["prescription_date"], "%Y-%m-%d") < end]
 
-
-# @app.callback(
-#     Output("leftGraph", "figure"),
-#     [Input("leftGraph", "selectedData")])
-# def updateLeftGraph(selected_data):
-#     data=[
-#         {
-#             "x": [i for i in range(1, 10)],
-#             "y": [j for j in range(11, 20)]
-#         }
-#     ]
-#     return dict(data=data)
 
 @app.callback(
     Output("rightGraph", "figure"),
-    [Input("leftGraph", "selectedData")])
-def updateLeftGraph(selected_data):
+    [Input("leftGraph", "relayoutData")])
+def updateLeftGraph(relayoutData):
 
-    if selected_data:
-        filtered=filterDf()
-    else:
-        filtered=prescriptions
     labels = list(set(prescriptions["complaint"]))
     data=[
         {
@@ -151,7 +83,21 @@ def updateLeftGraph(selected_data):
 
         }
     ]
-    return dict(data=data, layout={"title": "Complaints Breakdown"})
+    layout = dict(
+        title="Complaints Breakdown",
+        autosize=True,
+        margin=dict(
+            l=45,
+            r=45,
+            b=45,
+            t=55
+        ),
+        hovermode="closest",
+        plot_bgcolor="#F9F9F9",
+        paper_bgcolor="#F9F9F9",
+    )
+    return dict(data=data, layout=layout)
+
 
 if __name__ == '__main__':
     app.run_server(debug=True)
